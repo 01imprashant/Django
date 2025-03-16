b9dafeebd22e4a9679291fd8c4a403f6f136d6dd
Prashant Mishra <01imprashant@gmail.com>
2025-03-16 13:43:50 +0530

    update app

--- startproject/app/urls.py
+++ startproject/app/urls.py
@@ -2,8 +2,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('',views.app,name='app'),
+    path('', views.app,name='app'),
     path('<int:app_id>/', views.app_detail,name='app_detail'),
     path('app_stores/', views.app_store,name='app_store'),
+
 ]    
